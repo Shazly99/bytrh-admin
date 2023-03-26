ac435e00c247c614838c4591b6ebc100ec9076c4
Shazly99 <ahmed.y.elshazly@gmail.com>
2023-03-26 13:27:02 +0200

    translation into Arabic (blogs client-doctor)

--- src/Pages/Blogs/Client/BlogClient.jsx
+++ src/Pages/Blogs/Client/BlogClient.jsx
@@ -1,17 +1,22 @@
-import React, {useContext, useEffect, useRef, useState } from 'react';
-import { Pagination, Skeleton } from "@mui/material";
+import { Pagination } from "@mui/material";
 import Box from "@mui/material/Box";
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Col, Dropdown, DropdownButton, Form, Row, Table } from "react-bootstrap";
 import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import Img from "../../../assets/Img";
 import Icons from '../../../constants/Icons';
-import { apiheader, PostData } from '../../../utils/fetchData';
-import Component from '../../../constants/Component';
-import useSkeletonTable from '../../../utils/useSkeletonTable';
 import { VendersContext } from '../../../context/Store';
+import { apiheader, PostData } from '../../../utils/fetchData';
+import useSkeletonTable from '../../../utils/useSkeletonTable';
+import initialTranslation from "./Translation";
 
 const BlogClient = () => {
+    let { isLang } = useContext(VendersContext);
+    const [translate, setTranslate] = useState(initialTranslation)
+    const handelTranslate = () => {
+        setTranslate(initialTranslation)
+    }
     const animalRef = useRef();
 
     const [blogs, setBlogs] = useState(null)
@@ -20,7 +25,6 @@ const BlogClient = () => {
     const [showDropdown, setShowDropdown] = useState(false);
     const [isLoader, setIsloader] = useState(false);
     let { SkeletonTable, SkeletonSearchsingel, SkeletonFilterBlogs } = useSkeletonTable();
-    let { isLang } = useContext(VendersContext);
 
     const BlogsList = async () => {
         await PostData(`${process.env.REACT_APP_API_URL}/admin/clients/blogs`, { IDPage: page }, apiheader).then(({ data }) => {
@@ -48,12 +52,12 @@ const BlogClient = () => {
     const handleActionSelect = async (id, action) => {
         if (action === "PENDING" || action === "REJECTED" || action === "POSTED" || action === "REMOVED") {
             await ChangeBlogsStatus({ IDClientBlog: id, BlogStatus: action }).then((res) => {
-                toast.success('Updated Successfully', {
+                toast.success(<strong>{translate[isLang]?.blogDetails?.toastUpdate}</strong>, {
                     duration: 4000,
-                    position: 'top-center',
-                    icon: <Icons.UploadItem color='#3182CE' size={20} />,
+                    position: 'bottom-center',
+                    // icon: <Icons.UploadItem color='#3182CE' size={20} />,
                     iconTheme: {
-                        primary: '#0a0',
+                        primary: '#3182CE',
                         secondary: '#fff',
                     },
                 });
@@ -106,8 +110,10 @@ const BlogClient = () => {
 
     const handelSelectAnimalCategory = async (event) => {
         const selectedCountryId = event.target.value;
-        if (selectedCountryId === "Animal Category") {
+        if (selectedCountryId === "All") {
             BlogsList()
+        } else if (selectedCountryId === 'Select Category') {
+            return false
         } else {
             await animalcategories()
             let { data } = await PostData(`${process.env.REACT_APP_API_URL}/admin/clients/blogs`, { IDPage: page, IDAnimalCategory: selectedCountryId }, apiheader)
@@ -121,6 +127,7 @@ const BlogClient = () => {
         BlogsList(page)
         animalcategories()
         window.scrollTo(0, 0);
+        handelTranslate()
         return () => {
             BlogsList()
             animalcategories()
@@ -129,15 +136,15 @@ const BlogClient = () => {
     }, [page])
     useEffect(() => {
     }, [page, PagesNumber])
- 
-    return ( 
+
+    return (
         <>
             <div className="app__Users ">
                 <div className="app__Users-table ">
                     <div className="search-container">
                         {isLoader ? <>
                             <div className={`${isLang === 'ar' ? ' search__groupAr  ' : 'search__group'}  `}>
-                                <input type="text" placeholder="Search by Doctor Name,Email,Phone......" name="search" value={searchBlog} onChange={handleInputChange} />
+                                <input type="text" placeholder={translate[isLang]?.placeholder}  name="search" value={searchBlog} onChange={handleInputChange} />
                                 <button type="submit" onClick={handleSearchClick}>
                                     <Icons.Search color='#fff' size={25} />
                                 </button>
@@ -147,82 +154,34 @@ const BlogClient = () => {
                     <div className="app__addOrder-form  overflow-hidden">
                         <Row>
                             <Col xl={6} lg={6} md={6} sm={12} xs={12} >
-                                {isLoader ? <>
-                                    <h5 style={{ marginBottom: '15px', color: '#4A4A4A' }}>Filter by Blogs status :	</h5>
-                                    <div className='filter__group__stats row ' style={{ display: 'flex', gap: '0px', marginBottom: '25px' }}>
-                                        <label className='col active'>
-                                            {
-                                                selectedOption === "All" ?
-                                                    <input
-                                                        type="radio"
-                                                        name="filter"
-                                                        value="All"
-                                                        checked
-                                                        onChange={handleOptionChange}
-                                                        className={`inactive-radio form-check-input `}
-                                                    /> :
-                                                    <input
-                                                        type="radio"
-                                                        name="filter"
-                                                        value="All"
-                                                        checked={selectedOption === "All"}
-                                                        onChange={handleOptionChange}
-                                                        className={`inactive-radio form-check-input `}
-                                                    />
-                                            }
+                                <div className='filter__group__stats row ' style={{ display: 'flex', gap: '20px', marginBottom: '25px' }}>
+                                    {
+                                        translate[isLang]?.FilterStatus?.map((item, index) => (
+                                            <>
+                                                {isLoader ? <>
+                                                    <label className='col active d-flex justify-content-center align-item-center m-0 p-0 '  >
+                                                        <input
+                                                            type="radio"
+                                                            name="filter"
+                                                            value={item.value}
+                                                            checked={selectedOption === item.value}
+                                                            onChange={handleOptionChange}
+                                                            className="active-radio form-check-input"
+                                                        />
+                                                        {item.text}
+                                                    </label>
+                                                </> : SkeletonFilterBlogs(10, 90)}
+                                            </>
+                                        ))
+                                    }
 
-                                            All
-                                        </label>
-                                        <label className='col active'>
-                                            <input
-                                                className="  form-check-input"
-                                                type="radio"
-                                                name="filter"
-                                                value="POSTED"
-                                                checked={selectedOption === "POSTED"}
-                                                onChange={handleOptionChange}
-                                            />Posted
-                                        </label>        <label className='col active'>
-                                            <input
-                                                className="  form-check-input"
-                                                type="radio"
-                                                name="filter"
-                                                value="REJECTED"
-                                                checked={selectedOption === "REJECTED"}
-                                                onChange={handleOptionChange}
-                                            />Rejected
-                                        </label>
-                                        <label className='col active'>
-                                            <input
-                                                className="  form-check-input"
-                                                type="radio"
-                                                name="filter"
-                                                value="PENDING"
-                                                checked={selectedOption === "PENDING"}
-                                                onChange={handleOptionChange}
-                                            />
-                                            Pending
-                                        </label>
-                                        <label className='col active'>
-                                            <input
-                                                className="  form-check-input"
-                                                type="radio"
-                                                name="filter"
-                                                value="REMOVED"
-                                                checked={selectedOption === "REMOVED"}
-                                                onChange={handleOptionChange}
-                                            />Remove
-                                        </label>
-
-                                    </div>
-                                </> : SkeletonFilterBlogs()}
+                                </div>
                             </Col>
                             <Col xl={6} lg={6} md={6} sm={12} xs={12} className='Filter_by_Animal' >
                                 {isLoader ? <>
-                                    <h5 style={{ marginBottom: '15px', color: '#4A4A4A' }}>Filter by Animal Category :	</h5>
                                     <Form.Select aria-label="Default select example" ref={animalRef} onClick={handelSelectAnimalCategory}>
-                                        <option >Animal Category </option>
-
+                                        <option selected disabled hidden value={'Select Category'}> {translate[isLang]?.filter?.Category}</option>
+                                        <option value={'All'}  >{translate[isLang]?.filter?.allCategory}  </option>
                                         {
                                             animal?.map((item, index) => (
                                                 <option key={index} value={item?.IDAnimalCategory}>{item?.AnimalCategoryName}</option>
@@ -240,15 +199,11 @@ const BlogClient = () => {
                         <Table responsive={true} className='rounded-3 '>
                             <thead>
                                 <tr className='text-center  ' style={{ background: '#F9F9F9' }}>
-
-                                    <th>Client Name</th>
-                                    <th>Blog Title</th>
-                                    <th>Animal Category</th>
-                                    <th>Blog Status</th>
-                                    <th>Blog Visibility</th>
-                                    <th>Blog Date</th>
-                                    <th>Blog Stats</th>
-                                    <th>View Blog</th>
+                                    {
+                                        translate[isLang]?.TableHeader?.map((item, index) => (
+                                            <th key={index}>{item}</th>
+                                        ))
+                                    }
                                 </tr>
                             </thead>
                             <tbody className='text-center'>
@@ -280,8 +235,14 @@ const BlogClient = () => {
                                                                 ${item.BlogStatus == 'REJECTED' && 'txt_rejected'} 
                                                                 ${item.BlogStatus == 'POSTED' && 'txt_delivered'}
                                                                 ${item.BlogStatus == 'REMOVED' && 'txt_cancel'}`} >
-                                                        {item?.BlogStatus.charAt(0).toUpperCase() + item?.BlogStatus.slice(1).toLowerCase()}
-
+                                                        {
+                                                            translate[isLang].FilterStatus?.filter((itemfilter) => itemfilter.value === item?.BlogStatus)
+                                                                .map((status, index) => (
+                                                                    <React.Fragment key={index}>
+                                                                        {item?.BlogStatus === status.value ? status.text : ''}
+                                                                    </React.Fragment>
+                                                                ))
+                                                        }
                                                     </span>
                                                     <div className="delete">
                                                         <DropdownButton
@@ -293,25 +254,28 @@ const BlogClient = () => {
                                                             className="DropdownButton "
                                                             drop={'down-centered'}
                                                         >
-                                                            {
+                                                             {
                                                                 item.BlogStatus === 'PENDING' &&
                                                                 <>
-                                                                    <Dropdown.Item eventKey="POSTED">Post</Dropdown.Item>
-                                                                    <Dropdown.Item eventKey="REJECTED">Rejected</Dropdown.Item>
+                                                                    <Dropdown.Item  className={isLang === "ar" ? "dropdown-itemAr" : "dropdown-itemEn"} eventKey="POSTED">{translate[isLang].FilterStatus[3].text2}</Dropdown.Item>
+                                                                    <Dropdown.Item  className={isLang === "ar" ? "dropdown-itemAr" : "dropdown-itemEn"} eventKey="REJECTED">{translate[isLang].FilterStatus[2].text2}</Dropdown.Item>
                                                                 </>
                                                             }
                                                             {
                                                                 item.BlogStatus === 'REJECTED' &&
-                                                                <Dropdown.Item eventKey="POSTED">Post</Dropdown.Item>
+                                                                <Dropdown.Item  className={isLang === "ar" ? "dropdown-itemAr" : "dropdown-itemEn"} eventKey="POSTED">{translate[isLang].FilterStatus[3].text2}</Dropdown.Item>
                                                             }
                                                             {
                                                                 item.BlogStatus === 'REMOVED' &&
-                                                                <Dropdown.Item eventKey="POSTED">Post</Dropdown.Item>
+                                                                <Dropdown.Item  className={isLang === "ar" ? "dropdown-itemAr" : "dropdown-itemEn"} eventKey="POSTED">{translate[isLang].FilterStatus[3].text2}</Dropdown.Item>
                                                             }
                                                             {
                                                                 item.BlogStatus === 'POSTED' &&
-                                                                <Dropdown.Item eventKey="REMOVED">Removed</Dropdown.Item>
-                                                            }
+                                                                <Dropdown.Item  className={isLang === "ar" ? "dropdown-itemAr" : "dropdown-itemEn"} eventKey="REMOVED">{translate[isLang].FilterStatus[4].text2}</Dropdown.Item>
+                                                            } 
+
+                                                            
+
                                                         </DropdownButton>
 
                                                     </div>
@@ -346,7 +310,7 @@ const BlogClient = () => {
                                             <td >
                                                 <div>
                                                     <Link to={`/blogs/client/details/${item?.IDClientBlog}`}>
-                                                        <img src={Img.view}  />
+                                                        <img src={Img.view} />
                                                     </Link>
                                                 </div>
                                             </td>
@@ -361,7 +325,7 @@ const BlogClient = () => {
                 </div>
 
             </div>
-            <div className="pagination ">
+            <div className="pagination " dir="ltr">
                 <Box sx={{ margin: "auto", width: "fit-content", alignItems: "center", }}>
                     <Pagination count={pageCount} page={page} onChange={handleChange} />
                 </Box>
---
+++ src/Pages/Blogs/Client/Translation.js
@@ -0,0 +1,89 @@
+let initialTranslation = {
+    "ar": {
+        placeholder: 'البحث بالإسم  أو البريد الإلكتروني أو الهاتف .....', 
+        FilterStatus: [
+            { value: "All", text: "الجميع" },
+            { value: "PENDING", text: " قيد الانتظار  "},
+            { value: "REJECTED", text: "تــم الرفض" ,text2:"رفــض" },
+            { value: "POSTED", text: "   تم النشر  " ,text2:'نـــشر'},
+            { value: "REMOVED", text: " تمت الإزالة  ",text2:"إزالـــة" }, 
+        ],
+        TableHeader: [ ,
+            " معلومات العميل ",
+            "عنوان المدونة     ",
+            "فئة الحيوان ",
+            "حالة المدونة ",
+            "رؤية المدونة	 ",
+            "تاريخ المدونة",
+            "حالة المدونة",
+            'تفاصيل المدونة'
+        ],  
+       
+        filter:{  
+            Category:' حدد فئة الحيوانات',
+            allCategory:'كل فئات الحيوانات'
+        },
+        blogDetails:{
+            nav1:'المدونات',nav2:'تفاصيل المدونة', 
+            galleryTitle:'معرض المدونة',
+            galleryBtn:'عــرض المزيد' ,
+            like:'الإعجابات',
+            comment:'تعليقات',
+            Visibility:'الرؤية',
+            nameAr:'اسم التصنيف (بالعربية)',
+            nameEn:'اسم التصنيف (بالإنجليزية)',
+            deleteBtn:'حذف',
+            deleteTitle:'حذف تعليق ',
+            cancel:'رجــوع',
+            toast:'تــم حــذف التعليق ',
+            toastUpdate:'تـم تحديث الحالـة',
+
+
+            
+        }
+
+    },
+    "en": {
+        placeholder:   'Search by client name or email or phone.....', 
+        FilterStatus: [
+            { value: "All", text: "All" },
+            { value: "PENDING", text: " Pending  " },
+            { value: "REJECTED", text: "Rejected" ,text2:"Rejected"},
+            { value: "POSTED", text: "   Posted  " ,text2:'Post'},
+            { value: "REMOVED", text: " Remove  " ,text2:"Remove"}, 
+        ],
+        TableHeader: [ ,
+            " Client Info ",
+            "Blog Title     ",
+            "Animal Category",
+            "Blog Status ",
+            "Blog Visibility	 ",
+            "Blog Date ",
+            "Blog Status",
+            "View"
+        ],   
+
+        filter:{  
+            Category:' Select Animal Category ',
+            allCategory:'All category of animals'
+        },
+        blogDetails:{
+            nav1:'Blogs',nav2:'Blog Details', 
+            galleryTitle:'BLog Gallery',
+            galleryBtn:'Show more' ,
+            like:'Likes',
+            comment:'Comments',
+            Visibility:'Visibility',
+            nameAr:'Category Name (Ar)',
+            nameEn:'Category Name (En)',
+            deleteBtn:'Delete',
+            deleteTitle:'Delete Comment ',
+            cancel:'Cancel',
+            toast:'The Comment Has Been Deleted ',
+            toastUpdate:'Updated Successfully',
+            
+        }
+    }
+}
+
+export default initialTranslation
